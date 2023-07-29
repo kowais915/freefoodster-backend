a83314939d96eeb715a32392bfc4a7bf7ea438e0
kowais915 <kowais915@gmail.com>
2023-07-29 17:25:20 +0500

    :card_file_box: Mongodb Connected

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
+PORT = process.env.PORT || 8000;
+
+
+
 const app = express();
+
 
 app.use(express.json())
 
-app.listen(3001, ()=>{
-  console.log("listening on port 3000");
-});
+//* conntecting to mongodb
+mongoose.connect(process.env.MONGODB_URI)
+  .then(()=>{
+    console.log("Successfully connected to MongoDB.");
+    app.listen(PORT, ()=>{
+      console.log(`Listening on ${PORT}`);
+    })
+  })
+
+
 
 app.get("/", (req, res)=>{
     res.json({msg: "Welcome to the FreeFoodster API"})
